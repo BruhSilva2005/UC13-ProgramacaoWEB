0be48e76f918c00e6a90ffa1788117f55ea73f8c
bruna <bruna.ssantos102@senacsp.edu.br>
2023-08-22 17:24:49 -0300

    endereco

--- endereco.php
+++ endereco.php
@@ -1,3 +1,22 @@
+<?php
+        if(isset($_POST['cep']) && !empty($_POST['cep'])){
+             
+            $cep = $_POST['cep'];
+            $resultado = file_get_contents("http://viacep.com.br/ws/$cep/json/");
+
+            $dadosEndereço = json_decode($resultado, true);
+
+            var_dump($dadosEndereço);
+        }else{
+            header("location:buscar-endereco.php");
+            exit;
+        }
+
+?>
+
+
+
+
 <html>
     <head>
         <meta charset="utf-8">
@@ -6,10 +25,11 @@
     <body>
         <h1>Endereço</h1>
         <hr>
-        <P>CEP:17512-792</P>
-        <p>Logradouro: Rua Paraiba</p>
-        <p>Bairro: Centro</p>
-        <p>Cidade:Marilia</p>
-        <p>Estado: SP</p>
+        <P>CEP:<?=$dadosEndereço['cep']?></P>
+        <p>Logradouro:<?=$dadosEndereço['logradouro']?></p>
+        <p>Bairro:<?=$dadosEndereço['bairro']?></p>
+        <p>Localidade:<?=$dadosEndereço['localidade']?></p>
+        <p>UF:<?$dadosEndereço['uf']?></p>
+        <p> DDD:<?$dadosEndereço['ddd']?><P>
     </body>
 </html>
