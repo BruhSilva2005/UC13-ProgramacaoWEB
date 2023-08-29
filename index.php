223a67973e15d35b6637cb9e37cb141ebdcbc2a9
bruna <bruna.ssantos102@senacsp.edu.br>
2023-08-29 17:17:10 -0300

    aula de terça

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+    <div class = "container">
+        <div class = "text-bg-primary pb-1 mt-3 text-center rounded">  
+            <h1>Bootstrap</h1> 
+        </div>
+        <div class = "row">
+            <div class ="p-3 mb-2 bg-warning text-dark col-12 col-md-4">
+                <h2>Coluna 1</h2>
+        </div>
+        <div class ="p-3 mb-2 bg-info text-dark col-5 col-md-4">
+            <h2>Coluna 2</h2>
+        </div>
+        <div class ="p-3 mb-2 bg-secondary text-white col-7 col-md-4 ">
+            <h2>Coluna 3</h2>
+            </div>
+            <div class ="p-3 mb-2 bg-danger text-white col-12 col-md-12">
+                <h2>Coluna 4</h2>
+        </div>
+        <div class ="p-3 mb-2 bg-dark text-white col-12 col-md-6">
+                <h2>Coluna 5</h2> 
+            </div>
+            <div class="bg-danger-subtle col-md-6 col-12 d-none d-md-block ">
+                <h2>Coluna 6</h2>
+            </div>
+        <div class ="p-3 mb-2 bg-warning text-dark col-12 col-md-12">
+                <h2>Coluna 7</h2>
+        </div>
+        </div>
+    </div>
+   
+
+ 
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
